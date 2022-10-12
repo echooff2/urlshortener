433071546c33f0a3a1d4bf0bd8b815faea3a3114
echooff2 <23408914+echooff2@users.noreply.github.com>
2022-10-12 17:06:31 +0200

    change unwrap into proper error handling in index route

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,7 +1,12 @@
-use actix_web::{get, Responder};
 use actix_files::NamedFile;
+use actix_web::{error as web_error, get};
 
 #[get("/")]
-pub async fn index() -> impl Responder {
-    NamedFile::open_async("public/index.html").await.unwrap()
+pub async fn index() -> web_error::Result<NamedFile> {
+    Ok(NamedFile::open_async("public/index.html")
+        .await
+        .map_err(|err| {
+            log::error!("error while getting file at path 'public/index.html', err: {err}");
+            web_error::ErrorInternalServerError("Internal Server Error!")
+        })?)
 }
